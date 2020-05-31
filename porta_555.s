363b57de68c7255859452adc8413b35e8b293946
Jeremy Surma <59543358+JNSurma@users.noreply.github.com>
2020-05-31 11:37:48 -0500

    Add files via upload

---
+++ porta_555.s
@@ -0,0 +1,118 @@
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+
+E  = %10000000
+RW = %01000000
+RS = %00100000
+
+  .org $8000
+
+reset:
+  ldx #$ff	; Setup stack pointer
+  txs
+
+  jsr lcd_init
+
+  ldx #0
+  		; try to figure out now to send "message" to subroutine
+  jsr print
+
+  lda #%11111110    ; Set all pins on port A to output except last (555 timer input)
+  sta DDRA
+
+loop:
+  jsr delay
+  lda #%00010100
+  sta PORTA
+  jsr delay
+  lda #%00001010
+  sta PORTA
+  jmp loop
+
+message: .asciiz "Blinky Lights!!!"
+
+delay:
+  pha
+wait_on:
+  lda PORTA
+  and #%00000001
+  bne wait_on
+wait_off:
+  lda PORTA
+  and #%00000001
+  beq wait_off
+  pla
+  rts
+
+print:
+  lda message,x
+  beq print_end
+  jsr print_char
+  inx
+  jmp print
+print_end:
+  rts
+
+lcd_init:
+  lda #%11111111 ; Set all pins on port B to output
+  sta DDRB
+  lda #%11100000 ; Set top 3 pins on port A to output
+  sta DDRA
+
+  lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
+  jsr lcd_instruction
+  lda #%00001110 ; Display on; cursor on; blink off
+  jsr lcd_instruction
+  lda #%00000110 ; Increment and shift cursor; don't shift display
+  jsr lcd_instruction
+  lda #$00000001 ; Clear display
+  jsr lcd_instruction
+  rts
+
+lcd_wait:
+  pha
+  lda #%00000000  ; Port B is input
+  sta DDRB
+lcdbusy:
+  lda #RW
+  sta PORTA
+  lda #(RW | E)
+  sta PORTA
+  lda PORTB
+  and #%10000000
+  bne lcdbusy
+
+  lda #RW
+  sta PORTA
+  lda #%11111111  ; Port B is output
+  sta DDRB
+  pla
+  rts
+
+lcd_instruction:
+  jsr lcd_wait
+  sta PORTB
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  lda #E         ; Set E bit to send instruction
+  sta PORTA
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  rts
+
+print_char:
+  jsr lcd_wait
+  sta PORTB
+  lda #RS         ; Set RS; Clear RW/E bits
+  sta PORTA
+  lda #(RS | E)   ; Set E bit to send instruction
+  sta PORTA
+  lda #RS         ; Clear E bits
+  sta PORTA
+  rts
+
+  .org $fffc
+  .word reset
+  .word $0000
