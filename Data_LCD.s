657f8a6db26862bfb40995c6757b6118d8e0fbfb
Jeremy Surma <59543358+JNSurma@users.noreply.github.com>
2020-07-02 17:07:42 -0500

    Add files via upload
    Control LCD directly from data lines.

---
+++ Data_LCD.s
@@ -0,0 +1,84 @@
+LCD = $5000
+LCD0 = $5000
+LCD1 = $5001
+
+  .org $8000
+
+reset:
+  ldx #$ff	; Setup stack pointer
+  txs
+
+  jsr lint
+  jsr LCDSTRING  
+
+loop:
+  jmp loop
+
+
+message: .asciiz "Stinky Lights!!!"
+
+lcd2busy:
+  pha
+lcd2wait:
+  lda LCD0
+  and #$80
+  bne lcd2wait
+  pla
+  rts
+  
+lint:
+  lda #$38
+  sta LCD0
+  jsr lcd2busy
+  
+  lda #$06
+  sta LCD0
+  jsr lcd2busy
+  
+  lda #$0E
+  sta LCD0
+  jsr lcd2busy
+  
+  lda #$01
+  sta LCD0
+  jsr lcd2busy
+  
+  lda #$80
+  sta LCD0
+  jsr lcd2busy
+  rts
+
+LCDPRINT:
+  pha
+  sta LCD1
+  jsr lcd2busy
+  lda LCD0
+  and #$7F
+  cmp #$14
+  bne LCDPRINT0
+  lda #$C0
+  sta LCD0
+  jsr lcd2busy
+LCDPRINT0:
+  pla
+  rts
+  
+LCDSTRING:
+  pha
+  tya
+  ldy #$00
+LCDSTR0
+  lda message,y
+  beq LCDSTR1
+  jsr LCDPRINT
+  iny
+  bne LCDSTR0
+LCDSTR1:
+  pla
+  tay
+  pla
+  rts
+
+  .org $fffc
+  .word reset
+  .word $0000
